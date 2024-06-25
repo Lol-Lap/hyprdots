18da4a583ca0684d3319f44fedb8b79f500103fd
Lol-Lap <120711779+Lol-Lap@users.noreply.github.com>
2024-06-25 16:00:41 +0300

    Update gamemode.sh

--- Configs/.local/share/bin/gamemode.sh
+++ Configs/.local/share/bin/gamemode.sh
@@ -19,10 +19,10 @@ if [ $HYPRGAMEMODE = 1 ]; then
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0"
+        keyword general:gaps_in 3;\
+        keyword general:gaps_out 8;\
+        keyword general:border_size 2;\
+        keyword decoration:rounding 10"
 	exit
 else
 	hyprctl reload
